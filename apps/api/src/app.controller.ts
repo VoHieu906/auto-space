a2d0c0902b358927aaedd64f63088862fd321bc9
VoHieu906 <vohieutd09@gmail.com>
2025-02-14 10:11:35 +0700

    Add validation script

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { add } from '@autospace/sample-lib';
 
 @Controller()
 export class AppController {
@@ -7,6 +8,6 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return `hello ${add(12, 2)}`;
   }
 }
