1009e2a8fc516d9132858f6e6cf997938850b5f9
VoHieu906 <vohieutd09@gmail.com>
2025-02-14 15:46:53 +0700

    basic graphql demo

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,13 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       introspection: true,
+      fieldResolverEnhancers: ['guards'],
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      buildSchemaOptions: {
+        numberScalarMode: 'integer',
+      },
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
