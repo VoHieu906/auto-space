1792e97f8927e368fbcdab3b01df0738e1638f3f
VoHieu906 <vohieutd09@gmail.com>
2025-02-19 10:45:08 +0700

    GraphQl + rest codegen demo / swagger for api

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,22 +4,35 @@ import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
-import { UsersModule } from './users/users.module';
 
+import { PrismaModule } from './common/prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from './models/users/users.module';
+
+//Todo: move this to util lib
+const MAX_AGE = 24 * 60 * 60;
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: MAX_AGE }
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       introspection: true,
       fieldResolverEnhancers: ['guards'],
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       buildSchemaOptions: {
-        numberScalarMode: 'integer',
-      },
+        numberScalarMode: 'integer'
+      }
     }),
-    UsersModule,
+    PrismaModule,
+    UsersModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
