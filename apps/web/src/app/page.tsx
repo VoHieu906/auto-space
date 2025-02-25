8a8ac2d0cb91435cf440e86470aad08163507ec0
VoHieu906 <vohieutd09@gmail.com>
2025-02-25 10:29:35 +0700

    demo typesafe queries and mutation with GrqphQL codegen

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,4 +1,18 @@
+"use client";
 import { add } from "@autospace/sample-lib";
+import { useQuery } from "@apollo/client";
+import { CompaniesDocument } from "@autospace/network/src/gql/generated";
 export default function Home() {
-  return <main>Home {add(4, 3)}</main>;
+  const { data } = useQuery(CompaniesDocument);
+  console.log(data);
+  return (
+    <main>
+      Home {add(4, 3)}
+      <div>
+        {data?.companies.map((company) => (
+          <div key={company.id}>{company.displayName}</div>
+        ))}
+      </div>
+    </main>
+  );
 }
