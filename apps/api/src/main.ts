c361c327000f6e87c9733fc83258d10fd92472cf
VoHieu906 <vohieutd09@gmail.com>
2025-03-17 08:22:30 +0700

    modify the port of main

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
+const port = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
@@ -20,6 +20,6 @@ You might also need to use the <a target="_blank" href="https://studio.apollogra
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/', app, document);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
